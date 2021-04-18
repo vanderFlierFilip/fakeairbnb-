538d7d22871c7d1a5ab20f3f7e7b9bccb1908612
vanderFlierFilip <f.vanderflier@gmail.com>
2021-04-18 18:44:46 +0200

    added rental detail component, rental service and rental model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
+import { RentalModule } from './rental/rental.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header/header.component';
 
 
+const routes: Routes = [
+  { path: '', redirectTo: '/rentals', pathMatch: 'full' },
+
+];
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    RentalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
